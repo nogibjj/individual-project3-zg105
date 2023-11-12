bcef19b6dde01d3d08921ecaf45101e19cb1e108
Jason-Guo1999 <106792458+Jason-Guo1999@users.noreply.github.com>
2023-11-12 21:07:21 +0000

    commit

--- main.py
+++ main.py
@@ -1,49 +1,6 @@
-"""
-Command line query script
-"""
-import sqlite3
-import click
-
-@click.command()
-@click.argument("database")
-def command_line(database):
-    '''
-        Hi, this is a command line tool to do some queries.
-        Give me your database name, I can create a user table 
-        and insert some users for you.
-        Enjoy it :)
-    '''
-    conn = sqlite3.connect("database")
-    cursor = conn.cursor()
-
-    
-    # Create a table
-    click.echo("Creating table...")
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY,
-            username TEXT NOT NULL
-        )
-    """)
-    click.echo("inserting...")
-    cursor.execute("INSERT INTO users (username) VALUES ('zg105')")
-    cursor.execute("INSERT INTO users (username) VALUES ('yl794')")
-    cursor.execute("INSERT INTO users (username) VALUES ('aa809')")
-    # Delete
-    click.echo("Deleting...")
-    cursor.execute("DELETE from users where username = 'yl794'")
-    # Read
-    click.echo("Querying...")
-    cursor.execute("SELECT username FROM users")
-    users = cursor.fetchall()
-    
-    click.echo("current users:")
-    for user in users:
-        click.echo(user)
-
-    click.echo("Closing connection...")
-    conn.close()
-    click.echo("Finished")
+'''
+    main function here
+'''
 
 if __name__ == "__main__":
-    command_line()
+    a = 1
